0783a0edf8e9222c53ef59e8f5d9f65b0f85674b
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-11-24 22:32:36 -0300

    refactor: remove redundante redirect on home page

--- src/router.js
+++ src/router.js
@@ -1,21 +1,14 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
-import Assignment from './pages/Assignment'
-import Intro from './pages/Intro'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Assignment from './pages/Assignment';
+import Intro from './pages/Intro';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/intro">
-          <Intro />
-        </Route>
-        <Route path="/assignment">
-          <Assignment />
-        </Route>
-        <Route>
-          <Redirect to="/intro" />
-        </Route>
+        <Route exact path="/" component={ Intro } />
+        <Route path="/assignment" component={ Assignment } />
       </Switch>
     </BrowserRouter>
   )
